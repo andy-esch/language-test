677f2b958966d46e8e8527178bb5d84926962844
Otto Hasselblad <otto.hasselblad@gmail.com>
2011-09-15 13:06:25 -0400

    Transferred summary code into new file

---
+++ testResults.h
@@ -0,0 +1,16 @@
+/*
+ *  testResults.h
+ *  
+ *  Description: Header file to testResults.cpp
+ *
+ *
+ *  Created by Peter Eschbacher on 9/15/11.
+ *
+ */
+
+#include "wordData.h"
+#include "wordSet.h"
+#include <vector>
+using std::vector;
+
+void testResults(vector<wordSet> &, wordData[], int, int, bool);
