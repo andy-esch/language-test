ef1d78960885b81fd7c99d099a675c246c705cb7
Otto Hasselblad <otto.hasselblad@gmail.com>
2011-09-23 21:17:08 -0400

    Revised

--- src/class-test.cpp
+++ src/class-test.cpp
@@ -22,9 +22,26 @@ using std::vector;
 
 int main(int argc, char **argv)
 {
+    vector<Flashcard> cards;
     Flashcard fl1;
+    string str1("banksy"), str2("andy");
+    int numEntries;
+    for (int ii = 0; ii < 4; ii++)
+    {
+        fl1.input(str1,str2);
+        cards.push_back(fl1);
+        str1 += "1";
+        str2 += "2";
+    }
+    numEntries = cards.size();
+    for (int ii = 0; ii < numEntries; ii++)
+    {
+        cards[ii].updateInfo(numEntries);
+        cout << "Card " << ii+1 << ": " << endl;
+        cards[ii].output();
+        cout << "-------------------" << endl;
+    }
     
-    fl1.output();
 
     return 0;
 }
