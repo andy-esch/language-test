d243763efd673ed2ffe5263e2442e48565f4c29f
Kate Sugden <katikins@gmail.com>
2011-10-05 00:02:07 +0100

    Began SmartPicker class to hold card choice algorithms

---
+++ src/SmartPicker.cpp
@@ -0,0 +1,47 @@
+/*
+ *  SmartPicker.cpp
+ *  
+ *  Description:
+ *
+ *
+ *  Created by KEPS on 4/10/11.
+ *
+ */
+
+#include "SmartPicker.h"
+
+
+unsigned int SmartPicker::nextIndex(vector<Flashcard> cards)
+{
+  if(currentIndex==cards.size()-1)
+    return 0;
+  return currentIndex++
+}
+
+
+unsigned int SmartPicker::leastPickedIndex(vector<Flashcard> cards)
+{
+  vector<int> asked;
+  srand(time(0));
+  for(int i=0;i<card.size();i++)
+    {
+      asked.push_back(cards[i].numAsked);
+    }
+  std::sort(asked.begin(), asked.end());
+  int leastPicked = asked[0];
+  int count=0;
+  while(asked[count]<=leastPicked){
+    count++;
+  }
+  int indexChoice = rand()%count;
+  if(indexChoice==currentIndex)
+    return nextIndex();
+  setCurrentIndex(indexChoice);
+  return indexChoice;  
+}
+
+
+void SmartPicker::setCurrentIndex(int index)
+{
+  currentIndex=index;
+}
